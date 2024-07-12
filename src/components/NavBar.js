4ff487eb01ff4ff2ef07a88c5ac7b858f7743c7c
ceeoreo <ceoraford@gmail.com>
2024-07-11 21:36:05 -0400

    Remove cart and checkout page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,7 +13,6 @@ export default function NavBar() {
       {user && (
         <>
           <li><Link href="/profile">Profile</Link></li>
-          <li><Link href="/cart">Cart</Link></li>
         </>
       )} 
 			<li><AuthButtons /></li>     
